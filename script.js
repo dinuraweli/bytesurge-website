d79966ba3ab79fd7b4416bd3046bcce21819cc9a
dinuraweli <dinurawelikala@gmail.com>
2025-07-23 00:02:34 +0530

    Added new folder and files

---
+++ script.js
@@ -0,0 +1,33 @@
+
+  tsParticles.load("tsparticles", {
+    fullScreen: {
+      enable: false,
+      zIndex: 0
+    },
+    particles: {
+      number: {
+        value: 60
+      },
+      size: {
+        value: 3
+      },
+      move: {
+        enable: true,
+        speed: 1
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      color: {
+        value: "#ffffff"
+      }
+    },
+    background: {
+      color: "transparent"
+    }
+  });
+
